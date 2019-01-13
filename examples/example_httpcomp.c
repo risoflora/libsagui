760ee5470be1eaf8c85328d21a27938502b71238
silvioprog <silvioprog@gmail.com>
2019-01-13 00:26:48 -0300

    Added very simple example for how compress HTTP data using deflate.

---
+++ examples/example_httpcomp.c
@@ -0,0 +1,59 @@
+/*                         _
+ *   ___  __ _  __ _ _   _(_)
+ *  / __|/ _` |/ _` | | | | |
+ *  \__ \ (_| | (_| | |_| | |
+ *  |___/\__,_|\__, |\__,_|_|
+ *             |___/
+ *
+ *   –– cross-platform library which helps to develop web servers or frameworks.
+ *
+ * Copyright (c) 2016-2019 Silvio Clecio <silvioprog@gmail.com>
+ *
+ * This file is part of Sagui library.
+ *
+ * Sagui library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Sagui library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Sagui library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdlib.h>
+#include <sagui.h>
+
+/* NOTE: Error checking has been omitted to make it clear. */
+
+#define PAGE "<html><head><title>Hello world</title></head><body>Hello world</body></html>"
+#define CONTENT_TYPE "text/html; charset=utf-8"
+
+static void req_cb(__SG_UNUSED void *cls, __SG_UNUSED struct sg_httpreq *req, struct sg_httpres *res) {
+    struct sg_strmap **headers;
+    const char *header;
+    if ((headers = sg_httpreq_headers(req)) && (header = sg_strmap_get(*headers, "Accept-Encoding")) &&
+        strcasestr(header, "deflate"))
+        sg_httpres_zsendbinary(res, PAGE, strlen(PAGE), CONTENT_TYPE, 200);
+    else
+        sg_httpres_sendbinary(res, PAGE, strlen(PAGE), CONTENT_TYPE, 200);
+    headers = sg_httpres_headers(res);
+    sg_strmap_set(headers, "Content-Encoding", "deflate");
+}
+
+int main(void) {
+    struct sg_httpsrv *srv = sg_httpsrv_new(req_cb, NULL);
+    if (!sg_httpsrv_listen(srv, 0 /* 0 = port chosen randomly */, false)) {
+        sg_httpsrv_free(srv);
+        return EXIT_FAILURE;
+    }
+    fprintf(stdout, "Server running at http://localhost:%d\n", sg_httpsrv_port(srv));
+    fflush(stdout);
+    getchar();
+    sg_httpsrv_free(srv);
+    return EXIT_SUCCESS;
+}
