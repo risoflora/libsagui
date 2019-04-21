c87e0a095e846a621f3a6ac6c8e75c6d5b8b1f81
silvioprog <silvioprog@gmail.com>
2019-04-21 04:08:30 -0300

    Fixed broken test reported by travis.

--- test/test_utils.c
+++ test/test_utils.c
@@ -275,8 +275,8 @@ static void test_mm_set(void) {
     mm_tester = 0;
     ASSERT(sg_malloc(123) == &mm_tester);
     ASSERT(mm_tester == 123);
-    ASSERT(sg_realloc(&mm_tester, 10) == &mm_tester);
-    ASSERT(mm_tester == 133);
+    ASSERT(sg_realloc(&mm_tester, 456) == &mm_tester);
+    ASSERT(mm_tester == 456);
     sg_free(&mm_tester);
     ASSERT(mm_tester == 0);
 
