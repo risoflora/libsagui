c0711e877a2dce13e40bd8eff15e48663d1817db
silvioprog <silvioprog@gmail.com>
2019-05-19 23:50:05 -0300

    Improved logic which waits for the clients before server is shutting down.

--- src/sg_httpsrv.c
+++ src/sg_httpsrv.c
@@ -200,6 +200,7 @@ bool sg_httpsrv_listen(struct sg_httpsrv *srv, uint16_t port, bool threaded) {
 }
 
 int sg_httpsrv_shutdown(struct sg_httpsrv *srv) {
+    const union MHD_DaemonInfo *info;
     MHD_socket fd;
     if (!srv)
         return EINVAL;
@@ -214,7 +215,8 @@ int sg_httpsrv_shutdown(struct sg_httpsrv *srv) {
                 shutdown(fd, SHUT_RDWR);
                 close(fd);
 #endif
-                while (MHD_get_daemon_info(srv->handle, MHD_DAEMON_INFO_CURRENT_CONNECTIONS)->num_connections > 0)
+                info = MHD_get_daemon_info(srv->handle, MHD_DAEMON_INFO_CURRENT_CONNECTIONS);
+                while (info->num_connections > 0)
                     usleep(100 * 1000);
             }
         }
